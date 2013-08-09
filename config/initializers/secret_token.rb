6b1d5dfd0562c4c41219e9a462b668d55e4cb71c
Dezvoltare <razvanbigiu@ubuntu.(none)>
2013-08-09 13:09:00 +0300

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReAd::Application.config.secret_token = '1dcfdbfcf7f584777824ec8288b7a97ae033f3fa64d6ec5335143940bb2794d9aa73699384e6061fff3f363e7318f94e07a6b9314963debf92c3b695eea55004'
